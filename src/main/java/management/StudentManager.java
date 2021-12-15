efe7101ab28cdc1da3a440a5510be350e88ec7cd
Dennis Olsson <dennisolofolsson@live.se>
2021-12-15 10:43:11 +0100

    newStudentsMethods

--- src/main/java/management/StudentManager.java
+++ src/main/java/management/StudentManager.java
@@ -60,7 +60,7 @@ public class StudentManager {
             System.out.println("=====================");
             System.out.println("1.Update student's name");
             System.out.println("2.Update student's gender");
-            System.out.println("3.Update student's gender");
+            System.out.println("3.Update student's age");
             System.out.println("4.Update all of the above");
             System.out.println("0.Back to student menu");
 
@@ -75,8 +75,91 @@ public class StudentManager {
 
                     updateStudentsName(id,newName);
                     break;
+
+                case 2:
+                    System.out.print("new gender:");
+                    String newGender = sc.nextLine();
+
+                    updateStudentsGender(id,newGender);
+                    break;
+
+                case 3:
+                    System.out.print("new age:");
+                    int newAge = sc.nextInt();
+                    sc.nextLine();
+
+                    updateStudentsAge(id,newAge);
+                    break;
+
+                case 4:
+                    System.out.print("new name:");
+                    newName = sc.nextLine();
+
+                    System.out.print("new gender:");
+                    newGender = sc.nextLine();
+
+
+                    System.out.print("new age:");
+                    newAge = sc.nextInt();
+                    sc.nextLine();
+
+                    updateAllStudentInfo(id,newName,newGender,newAge);
+                    break;
+
+                case 0:
+                    loop = false;
+                    break;
+
+                default:
+                    System.out.println("There's no such option, try again");
             }
         }
+    }
+
+    private static void updateAllStudentInfo(int id, String newName, String newGender, int newAge) {
+        EntityManager em = emf.createEntityManager();
+
+        Student student = em.find(Student.class, id);
+
+        em.getTransaction().begin();
+
+        student.setName(newName);
+
+        student.setGender(newGender);
+
+        student.setAge(newAge);
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    private static void updateStudentsAge(int id, int newAge) {
+        EntityManager em = emf.createEntityManager();
+
+        Student student = em.find(Student.class, id);
+
+        em.getTransaction().begin();
+
+        student.setAge(newAge);
+
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    private static void updateStudentsGender(int id, String newGender) {
+        EntityManager em = emf.createEntityManager();
+
+        Student student = em.find(Student.class, id);
+
+        em.getTransaction().begin();
+
+        student.setGender(newGender);
+
+        em.getTransaction().commit();
+
+        em.close();
     }
 
     private static void updateStudentsName(int id, String newName) {
